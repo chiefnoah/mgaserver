f84a9c8c75e53eb0534b1ed15e4a8897b51f77a7
Noah Pederson <Noah@Noahs-iMac.local>
2014-11-22 11:55:21 -0600

    Added default framework for a Node.JS express app

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
-//Dependencies
 var express = require('express');
-var connect = require('connect');
-var app = express();
-var port = process.env.PORT || 8080;
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
-app.use(connect.cookieParser());
-app.use(connect.logger('dev'));
-app.use(connect.bodyParser());
-app.use(connect.json());
-app.use(connect.urlencoded());
-//Routes
-require('./routes/routes.js');
-app.listen(port);
-console.log('Server is running on port: ' + port);
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
