3d77d13d76b99dddb5c35991a0bf76c03a33d029
Noah Pederson <pedersonnoah@gmail.com>
2014-11-22 23:21:14 -0600

    Added servername, port, and db path properties

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //Name of the server
+  name: 'Mga Server',
+  //Port the server will run on
+  port: '3000',
+  //Path to directory containing the .cbz and .cbr files
+  path: ''
+
+
+  //TODO: Add more config options
+
+};
