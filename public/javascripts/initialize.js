84b40930ac5f3394ea42edc4aab97024369ad0d5
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 16:59:21 -0600

    Switched from socket.io to AJAX to be more compatible with future non-browser clients

--- public/javascripts/initialize.js
+++ public/javascripts/initialize.js
@@ -1,12 +1,15 @@
 document.getElementById('socket_message').innerHTML = 'Not connected to server';
-var socket = io();
-
-socket.on('on connect', function(data) {  document.getElementById('socket_message').innerHTML = data.message;
-});
-
 
 //Set loadDb to execute on click
 //document.getElementById('load_database').onclick = loadDb();
 function loadDb() {
-  socket.emit('load db', {});
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      document.getElementById("socket_message").innerHTML = xmlhttp.responseText;
+    }
+  };
+  xmlhttp.open("GET", "/initialize/load_database", true);
+  xmlhttp.send();
 }
