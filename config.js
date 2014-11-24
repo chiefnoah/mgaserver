e8701f7c492550a56d3b3526fc0378e4ac5b3410
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 16:58:25 -0600

    Added more properites, including a users, and path for storing the database for users

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-
   //Name of the server
   name: 'Mga Server',
   //Port the server will run on
@@ -7,11 +6,21 @@ module.exports = {
   //Path to directory containing the .cbz and .cbr files
   path: './Comics/',
 
+
   database: {
     //This is the path to where the database is stored
     //You probably don't need to change this
-    path: './db/mga-1.db'
-  }
+    path: './db/mga-1.db',
+    user_path: './db/users.db'
+  },
   //TODO: Add more config options
+
+  //Define users here. The default user
+  users: [{
+    name: 'default',
+    password: 'password',
+    sync: false, //Syncronize comic read data
+
+  }]
 
 };
