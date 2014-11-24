9006e55c5b804eeead56cdd5b73ca5c4ad0e95f0
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 00:25:56 -0600

    Added properties for database path

--- config.js
+++ config.js
@@ -5,9 +5,13 @@ module.exports = {
   //Port the server will run on
   port: '3000',
   //Path to directory containing the .cbz and .cbr files
-  path: ''
+  path: './Comics/',
 
-
+  database: {
+    //This is the path to where the database is stored
+    //You probably don't need to change this
+    path: './db/mga-1.db'
+  }
   //TODO: Add more config options
 
 };
