f0d05c2e2b20ac4139d001b2d6115b816a6237d2
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 00:27:24 -0600

    now emits a 'load db' event for when a button is clicked

--- public/javascripts/initialize.js
+++ public/javascripts/initialize.js
@@ -1,9 +1,12 @@
-document.getElementById('socket_message').innerHTML = 'No message received';
+document.getElementById('socket_message').innerHTML = 'Not connected to server';
 var socket = io();
 
 socket.on('on connect', function(data) {  document.getElementById('socket_message').innerHTML = data.message;
 });
 
+
+//Set loadDb to execute on click
+//document.getElementById('load_database').onclick = loadDb();
 function loadDb() {
-  io.emit('load db');
+  socket.emit('load db', {});
 }
