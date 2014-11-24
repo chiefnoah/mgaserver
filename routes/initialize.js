b4db007fae3c3083bb418664ef269d857fe30162
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 17:00:05 -0600

    Added GET route for initalizing the databases (dev only)

--- routes/initialize.js
+++ routes/initialize.js
@@ -4,8 +4,14 @@ var config = require('../config.js');
 
 // Handle basic reqest at this path
 router.get('/', function(req, res) {
-  res.render('initialize', {db_path: config.path});
+  res.render('initialize', {
+    db_path: config.path
+  });
 });
 
-
+router.get('/load_database', function(req, res) {
+  console.log("Opening database");
+  var database = require('../util/database')(); //Just creating a database object automatically initializes it.
+  res.status(200).send('Initializing db');
+});
 module.exports = router;
