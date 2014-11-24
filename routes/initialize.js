92421252dbc61014b1587a1a83d71fd45823eef3
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 00:28:15 -0600

    no uses the config file for references to the database path

--- routes/initialize.js
+++ routes/initialize.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.js');
 
 // Handle basic reqest at this path
 router.get('/', function(req, res) {
-  res.render('initialize', {db_path: '<i>none</i>'});
+  res.render('initialize', {db_path: config.path});
 });
 
 
