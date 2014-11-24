dad9831301a593e3470df0396fd74fd05524e4f0
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 00:25:32 -0600

    Removed any reference to the autogenerated users route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var initialize = require('./routes/initialize');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/initialize', initialize);
 
 // catch 404 and forward to error handler
