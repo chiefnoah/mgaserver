6b14418c2a5f32aa64d570c95cfe995e51d0b741
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 00:30:01 -0600

    initializes database in response to the 'load db' event

--- sockets.js
+++ sockets.js
@@ -4,14 +4,18 @@ module.exports = function(io) {
     console.log('client connected');
     //ALL SOCKET EVENTS GO IN HERE
     socket.emit('on connect', {message: 'Established socket connection'});
+
+    socket.on('load db', function() {
+      //TODO: Code for generating the database from the provided path
+      console.log("Opening database");
+      var database = require('./util/database')(); //Just creating a database object automatically initializes it.
+    });
+
+
+
+
     io.on('disconnect', function() {
       console.log("client disconnected");
     });
-
-    io.on('load db', function() {
-      //TODO: Code for generating the SQLite3 database from the provided path
-    });
-
-
   });
 };
