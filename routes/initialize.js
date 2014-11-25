15cc480068e935ffc6b907806db731d08dd93f50
Noah Pederson <pedersonnoah@gmail.com>
2014-11-24 19:43:50 -0600

    Now initializes scanner

--- routes/initialize.js
+++ routes/initialize.js
@@ -12,6 +12,9 @@ router.get('/', function(req, res) {
 router.get('/load_database', function(req, res) {
   console.log("Opening database");
   var database = require('../util/database')(); //Just creating a database object automatically initializes it.
+
+  var scanner = require('../util/scanner')();
+  console.log(scanner);
   res.status(200).send('Initializing db');
 });
 module.exports = router;
