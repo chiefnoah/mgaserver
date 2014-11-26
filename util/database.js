f424f0dd0b592b1f1d176cee763f9e3df5b2e60f
Noah Pederson <pedersonnoah@gmail.com>
2014-11-26 00:57:32 -0600

    reformatted exports. Now exports comic_dbInsert function

--- util/database.js
+++ util/database.js
@@ -1,44 +1,76 @@
-module.exports = function() {
-  var config = require('../config');
-  var Datastore = require('nedb');
-  //Persistent database
-  var db = {};
-  db.comics = new Datastore({
-    filename: config.database.path
-  });
+    var config = require('../config');
+    var Datastore = require('nedb');
+    //Persistent database
+    var db = {};
 
-  db.users = new Datastore({
-    filename: config.database.user_path
-  });
-  //Memory database (for faster access) DOESN'T need to be loaded
-  //var mdb = new Datastore();
-  //Load the databases
-  db.comics.loadDatabase(function(err) {
-    if (err) console.log("Error loading comics database");
-    //This should really report the error on the webpage. I'll do it later
-    console.log("Comics database succesfully loaded");
-  });
+    db.series = new Datastore({
+      filename: config.database.series_path,
+      autoload: true
+    });
 
-  db.users.loadDatabase(function(err) {
-    if (err) console.log("Error loading users database");
-    //This should really report the error on the webpage. I'll do it later
-    console.log("Users database succesfully loaded");
-  });
+    db.comics = new Datastore({
+      filename: config.database.comics_path,
+      autoload: true
+    });
 
-  //All functions that I want to be accessable to the database object must be inside the return statement here
-  return {
-    //Expects and array of data to insert into the db.
-    dbInsert: function(data) {
-      db.insert(data, function(err, newDoc) {
-        if (err) console.log(err);
+    db.users = new Datastore({
+      filename: config.database.users_path,
+      autoload: true
+    });
+    /*
+    db.comics.ensureIndex({
+      fieldName: 'series_title',
+      unique: false
+    }, function(err) {
+      if (err) console.log(err);
+    });
+
+    db.comics.ensureIndex({
+      fieldName: 'chapter',
+      unique: false
+    }, function(err) {
+      if (err) console.log(err);
+    });
+
+    //Memory database (for faster access) DOESN'T need to be loaded
+    //var mdb = new Datastore();
+    //Load the databases
+    db.series.loadDatabase(function(err) {
+      if (err) console.log("Error loading series database: " + err);
+      //This should really report the error on the webpage. I'll do it later
+      //console.log("Series database succesfully loaded");
+    });
+
+    db.comics.loadDatabase(function(err) {
+      if (err) console.log("Error loading comics database: " + err);
+      //This should really report the error on the webpage. I'll do it later
+      //console.log("Comics database succesfully loaded");
+    });
+
+    db.users.loadDatabase(function(err) {
+      if (err) console.log("Error loading users database: " + err);
+      //This should really report the error on the webpage. I'll do it later
+      //console.log("Users database succesfully loaded");
+    });
+    */
+
+    //Expects a string with the name of the database and array of data to insert into the db.
+    var comics_dbInsert = function(data) {
+      //console.log(data);
+      db.comics.insert(data, function(err, newDoc) {
+        if (err) {
+          console.log(err);
+          return "Data not added to database 'comics'";
+        }
         for (var i = 0; i < newDoc.length; i++) {
           console.log(newDoc[i] + 'has been added to the db');
         }
+        return "Data was successfully saved to comics";
       });
-    }
+    };
+
+    module.exports = {
+      comics_dbInsert: comics_dbInsert
+    };
 
     //TODO: More CRUD operations here :D
-
-
-  };
-};
