f426569a88ae3fced0fab4b7f77328eeb561e5a8
Noah Pederson <pedersonnoah@gmail.com>
2014-11-26 00:56:21 -0600

    Added more properties

--- config.js
+++ config.js
@@ -4,14 +4,15 @@ module.exports = {
   //Port the server will run on
   port: '3000',
   //Path to directory containing the .cbz and .cbr files
-  path: './Comics/',
+  path: './Comics',
 
 
   database: {
     //This is the path to where the database is stored
     //You probably don't need to change this
-    path: './db/mga-1.db',
-    user_path: './db/users.db'
+    series_path: './db/mga-0.db',
+    comics_path: './db/mga-1.db',
+    users_path: './db/users.db'
   },
   //TODO: Add more config options
 
@@ -21,6 +22,21 @@ module.exports = {
     password: 'password',
     sync: false, //Syncronize comic read data
 
-  }]
+  }],
+
+  scrapers: {
+    myanimelist: {
+      username: 'chiefnoah',
+      password: '11noah',
+
+      request_url: 'http://myanimelist.net/api/manga/search.xml',
+      requst_variable: 'q'
+        //See http://myanimelist.net/modules.php?go=api for details regarding the API
+    },
+
+    comicvine: {
+      //TODO: Add stuff for comicvine scraper API
+    }
+  }
 
 };
