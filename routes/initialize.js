9f338c0a5fc9b8cd575a1d757a68e8fb6dbb805e
Noah Pederson <pedersonnoah@gmail.com>
2014-12-07 14:00:38 -0600

    Now checks if the data it receives is null and displays a message if it is

--- routes/initialize.js
+++ routes/initialize.js
@@ -12,8 +12,12 @@ router.get('/', function(req, res) {
 router.get('/load_database', function(req, res) {
   var scanner = require('../util/scanner')(function(data) {
     var output = '<table width="100%"border="1"><tr><th>Series</th><th>Chapter</th><th>Path</th></tr>';
-    for (var i = 0; i < data.length; i++) {
-      output += '<tr><td>' + data[i].series_title + '</td><td>' + data[i].chapter + '</td><td>' + data[i].relative_path + '</td>';
+    if (data) {
+      for (var i = 0; i < data.length; i++) {
+        output += '<tr><td>' + data[i].series_title + '</td><td>' + data[i].chapter + '</td><td>' + data[i].relative_path + '</td>';
+      }
+    } else {
+      output = '<p><i>No new files added</i></p>';
     }
     res.status(200).send(output);
   }); //Calls the scanner function. Returns the scanned comic metadata
