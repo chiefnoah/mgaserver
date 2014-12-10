bc2249e2a94da2094b976871bcb3180bb900107b
Noah Pederson <pedersonnoah@gmail.com>
2014-12-09 19:13:14 -0600

    Updated algorithm for scrapping comics. Now uses Levenshtein distance to choose a best match.

--- util/scrapers/myanimelist.js
+++ util/scrapers/myanimelist.js
@@ -6,6 +6,7 @@ var Manga = require('../series_metadata');
 var fs = require('fs');
 var request = require('request');
 var path = require('path');
+var levenshtein = require('levenshtein');
 
 
 var search_manga = function(series_title, callback) {
@@ -60,49 +61,66 @@ var search_manga = function(series_title, callback) {
 
 
   function findBestResult(objectList) {
+    var levenDis = [];
+    var name = series_title + ' Manga';
+    //Loop through all the returned objects and
     for (i = 0; i < objectList.length; i++) {
-      var reg = new RegExp(series_title, 'i');
-      if ((objectList[i].title[0].search(reg) > -1) && (objectList[i].type[0] === 'Manga' || objectList[i].type[0] === '')) {
-        var mData = objectList[i];
-        result.title = series_title;
-        result.cover = saveCover(mData.image[0]);
-        result.start_date = mData.start_date[0];
-        result.end_date = mData.end_date[0];
-        result.authors = [''];
-        result.artists = [''];
-        result.publisher = '';
-        result.status = mData.status[0];
-        result.genres = ['Manga'];
-        result.description = mData.synopsis[0];
-        result.chapters = mData.chapters[0];
-        result.volumes = mData.volumes[0];
-        result.rating = mData.score[0];
-        this.aliases = mData.synonyms;
+      var cLeven = new levenshtein(name, objectList[i].title[0] + ' ' + objectList[i].type[0]);
+      levenDis[i] = 100 - cLeven.distance;
+    }
+
+    //Find the index of the largest value in array
+    var max = levenDis[0];
+    var maxIndex = 0;
+    for (var z = 1; z < levenDis.length; z++) {
+      if (levenDis[z] > max) {
+        maxIndex = z;
+        max = levenDis[z];
       }
     }
 
-    function saveCover(imageURL) {
-      var fileURI = './public/images/series/' + path.basename(imageURL);
-      //console.log('downloading ' + imageURL + ' to ' + fileURI);
-
-      var download = function(uri, filename, callback) {
-        request.head(uri, function(err, res, body) {
-          //console.log('content-type:', res.headers['content-type']);
-          //console.log('content-length:', res.headers['content-length']);
-
-          request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-        });
-      };
-
-
-
-      download(imageURL, fileURI, function() {
-        //console.log('saved ' + imageURL + ' to ' + fileURI);
-      });
-      return fileURI;
-    }
-
+    //Set the values to the best match
+    var mData = objectList[maxIndex];
+    result.title = mData.title[0];
+    result.cover = saveCover(mData.image[0]);
+    result.start_date = mData.start_date[0];
+    result.end_date = mData.end_date[0];
+    result.authors = [''];
+    result.artists = [''];
+    result.publisher = '';
+    result.status = mData.status[0];
+    result.genres = ['Manga'];
+    result.description = mData.synopsis[0];
+    result.chapters = mData.chapters[0];
+    result.volumes = mData.volumes[0];
+    result.rating = mData.score[0];
+    this.aliases = mData.synonyms;
   }
+
+
+
+  function saveCover(imageURL) {
+    var fileURI = './public/images/series/' + path.basename(imageURL);
+    //console.log('downloading ' + imageURL + ' to ' + fileURI);
+
+    var download = function(uri, filename, callback) {
+      request.head(uri, function(err, res, body) {
+        //console.log('content-type:', res.headers['content-type']);
+        //console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+      });
+    };
+
+
+
+    download(imageURL, fileURI, function() {
+      //console.log('saved ' + imageURL + ' to ' + fileURI);
+    });
+    return fileURI;
+  }
+
+
 };
 
 module.exports = {
