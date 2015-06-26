b75910079652a374ddabc4affb18555965ccbf19
Noah Pederson <pedersonnoah@gmail.com>
2015-06-26 02:58:26 -0500

    Comic API (should be) fully compatible with ComicStreamer

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var querystring = require('querystring');
+var path = require('path');
 var db = require('../util/YDBHandler');
 var config = require('../util/settings_handler').getConfig();
 
@@ -23,13 +25,39 @@ router.get("/", function(req, res, next) {
                 next();
         }
     } else {
-      next();
+        next();
     }
 });
 
 //TODO: restructure to accept query arguments
 router.get('/comiclist', function(req, res) {
-    db.getComicInfo([], function(err, rows) {
+    res.type('application/json');
+    var queryParams = {};
+
+
+    queryParams.keyphrase = req.query.keyphrase;
+    queryParams.series = req.query.series;
+    queryParams.path = req.query.path;
+    queryParams.folder = req.query.folder;
+    queryParams.title = req.query.title;
+    queryParams.start_date = req.query.start_date;
+    queryParams.end_date = req.query.end_date;
+    queryParams.added_since = req.query.added_since;
+    queryParams.modified_since = req.query.modified_since;
+    queryParams.lastread_since = req.query.lastread_since;
+    queryParams.order = req.query.order;
+    queryParams.character = req.query.character;
+    queryParams.team = req.query.team;
+    queryParams.location = req.query.location;
+    queryParams.storyarc = req.query.storyarc;
+    queryParams.volume = req.query.volume;
+    queryParams.publisher = req.query.publisher;
+    queryParams.credit = req.query.credit;
+    queryParams.tag = req.query.tag;
+    queryParams.genre = req.query.genre;
+
+
+    db.getComicInfo(queryParams, function(err, rows) {
         if (err) console.log(err);
         res.send(JSON.stringify({
             total_count: rows.length,
@@ -42,12 +70,13 @@ router.get('/comiclist', function(req, res) {
 //Searches comics for a specific id
 router.get('/comic/:id', function(req, res) {
     //console.log(req.params.id);
-
-    db.getComicInfo(["I.id = " + req.params.id], function(err, rows) {
+    var params = {};
+    params.id = req.params.id;
+    db.getComicInfo(params, function(err, rows) {
         if (err) {
-          console.log(err); 
-          res.status(404).end();
-          return;
+            console.log(err);
+            res.status(404).end();
+            return;
         }
         res.send(JSON.stringify({
             total_count: rows.length,
@@ -60,56 +89,71 @@ router.get('/comic/:id', function(req, res) {
 //TODO: convert this to comicstreamer compatible
 //This should point to the file to the comic with the ID passed as a parameter
 router.get('/comic/:id/file', function(req, res) {
-    var comicID = req.query.id;
-    //This should only ever return one document.
-    //if (comicID) {
-    if (false) {
-        db.comics.findOne({
-            _id: comicID
-        }, function(err, doc) {
-            if (doc) {
-                console.log(doc.relative_path);
-                //Set the mime type to a default value
-                var mimeType = 'application/octet-stream';
-                if (path.extname(doc.relative_path) === '.cbz') {
-                    mimeType = 'application/x-cbz';
-                } else if (path.extname(doc.relative_path) === '.cbr') {
-                    mimeType = 'application/x-cdisplay';
+    var id = req.params.id;
+
+    db.getComicFile(id, function(err, row) {
+
+        if (err) {
+            console.log(err);
+        }
+        if (row) {
+
+            row.path = (config.path + row.path).replace(/\//g, "\\");
+            console.log("PATH: " + row.path);
+
+            var mimeType = 'application/octet-stream';
+            if (path.extname(row.path) === '.cbz') {
+                mimeType = 'application/x-cbz';
+            } else if (path.extname(row.path) === '.cbr') {
+                mimeType = 'application/x-cdisplay';
+            }
+
+            var options = {
+                headers: {
+                    'x-timestamp': Date.now(),
+                    'x-sent': true,
+                    'Content-Type': mimeType,
+                    'Content-Disposition': 'filename=' + path.basename(row.path)
+                }
+            };
+            res.sendFile(row.path, options, function(err) {
+                if (err) {
+                    console.log(err);
+                    res.status(err.status).end();
+                } else {
+                    console.log("Sent: " + row.path);
                 }
 
-                var options = {
-                    headers: {
-                        'x-timestamp': Date.now(),
-                        'x-sent': true,
-                        'Content-Type': mimeType,
-                        'Content-Disposition': 'filename=' + path.basename(doc.relative_path)
-                    }
-                };
-                res.sendFile(doc.relative_path, options, function(err) {
-                    if (err) {
-                        console.log(err);
-                        res.status(err.status).end();
-                    } else {
-                        console.log('Sent:', doc.relative_path);
-                    }
-                });
-            }
-        });
-    } else {
-        console.log('ERROR: no ID');
-        res.send(JSON.stringify({
-            error: 'No ID'
-        }));
-    }
+            });
+        } else {
+            res.status(404).end();
+            console.log("Error: Invalid ID - the db query returned nothing");
+        }
+    });
 });
 
 router.get('/comic/:id/thumbnail', function(req, res, next) {
-    db.getComicInfo("SELECT * FROM comic_info WHERE id=" + req.params.id, function(err, rows) {
-        console.log(err);
+    var queryParams = {};
+    queryParams.id = req.params.id;
+    db.getComicInfo(queryParams, function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
         if (rows[0]) {
             var thumbnailPath = config.path + '/.yacreaderlibrary/covers/' + rows[0].hash + '.jpg';
+            thumbnailPath = thumbnailPath.replace(/\//g, "\\");
             console.log(thumbnailPath);
-            res.sendFile(thumbnailPath.replace('/', '\\'), function(err) {
+            var mimeType = 'image/jpeg';
+            var options = {
+                headers: {
+                    'x-timestamp': Date.now(),
+                    'x-sent': true,
+                    'Content-Type': mimeType,
+                    'Content-Disposition': 'filename=' + rows[0].hash + '.jpg'
+                }
+            };
+            res.sendFile(thumbnailPath, options, function(err) {
                 if (err) {
                     console.log(err);
                     res.status(err.status).end();
@@ -130,30 +174,57 @@ router.get('/comic/:id/page/:pagenum', function(req, res) {
 });
 
 router.get('/folders/0/:path', function(req, res) {
-  var path = "/" + req.params.path;
-  console.log("Path: " + path);
-  db.getFolders(path, function(result) {
-    res.send(JSON.stringify(result));
-  });
+    res.type('application/json');
+    var path = "/" + req.params.path;
+    console.log("Path: " + path);
+    db.getFolders(path, function(result) {
+        res.send(JSON.stringify(result));
+    });
+});
+
+router.get('/folders/0:path', function(req, res) {
+    res.type('application/json');
+    console.log("stupid path quirks...");
+    var path = querystring.unescape(req.params.path);
+    console.log("Path: " + path);
+    db.getFolders(path, function(result) {
+        res.send(JSON.stringify(result));
+    });
 });
 
 router.get('/folders/0/', function(req, res) {
-  var path = '/';
-  console.log("Path: " + path);
-  db.getFolders(path, function(result) {
-    res.send(JSON.stringify(result));
-  });
+    res.type('application/json');
+    var path = '/';
+    console.log("Path: " + path + "0");
+    db.getFolders(path, function(result) {
+        res.send(JSON.stringify(result));
+    });
 });
 
+
+//TEST
 router.get('/folders', function(req, res) {
-  var path = '/';
-  console.log("Path: " + path);
-  db.getFolders(path, function(result) {
-    res.send(JSON.stringify(result));
-  });
+    res.type('application/json');
+    //TODO: NOT hardcode this
+    var path = '/';
+    console.log("Path: root");
+    var root = {
+        current: "",
+        folders: [{
+            url_path: "/folders/0",
+            name: "root"
+        }],
+        comics: {
+            count: 0,
+            url_path: ""
+        }
+    };
+    res.send(JSON.stringify(root));
+
 });
 
 router.get('/version', function(req, res) {
+    res.type('application/json');
     res.send(JSON.stringify({
         server: "MgaServer v0.1",
         version: "0.0.7"
@@ -161,6 +232,7 @@ router.get('/version', function(req, res) {
 });
 
 router.get('/dbinfo', function(req, res) {
+    res.type('application/json');
     //TODO: return DB info. 
     res.send(JSON.stringify({
         Version: "test"
@@ -172,9 +244,9 @@ module.exports = router;
 
 //Quick check to see if an we have an empty object
 function isEmpty(obj) {
-  for(var prop in obj) {
-    if(obj.hasOwnProperties(prop))
-      return false;
-  }
-  return true;
+    for (var prop in obj) {
+        if (obj.hasOwnProperties(prop))
+            return false;
+    }
+    return true;
 }
--- util/YDBHandler.js
+++ util/YDBHandler.js
@@ -1,20 +1,120 @@
 var sqlite = require('sqlite3').verbose();
+var querystring = require('querystring');
 var config = require('./settings_handler').getConfig();
 var db = new sqlite.Database(config.path + '/.yacreaderlibrary/library.ydb');
+var moment = require('moment');
 
 var closeDB = function() {
     db.close();
 };
 
 var getComicInfo = function(params, callback) {
-    var sql = "SELECT * FROM comic_info AS I JOIN comic AS C ON I.id=C.comicInfoId";
-    if (params && params.length > 0) {
-        sql += " WHERE ";
-    }
-    for (var i = 0; i < params.length; i++) {
-        sql += params[i] + " AND ";
+    var sql = "SELECT comic.id as id, hash, folder.path as folderPath, comic.path as comicPath, writer, penciller, inker, colorist, letterer, coverArtist, volume as series, title, publisher, synopsis, notes as comments, genere as genre, numPages as page_count, number as issue, currentPage as lastread_page, read, storyArc, arcNumber as volume from comic LEFT JOIN comic_info ON comic.comicInfoId=comic_info.id LEFT JOIN folder ON comic.parentId=folder.id";
+
+    var where = "";
+
+    where += addQueryOnScalar("comic.id", params.id);
+
+    params.person = null;
+    params.role = null;
+    if (!isEmpty(params.credit)) {
+        var credit_info = params.credit.split(':');
+        if (credit_info[0].length != 0) {
+            params.person = credit_info[0];
+            if (credit_info.length > 1) {
+                params.role = credit_info[1];
+            }
+        }
     }
 
+    if (params.role) {
+        params.role = params.role.replace("*", "%");
+        if (params.person) {
+            params.person = params.person.replace("*", "%");
+            where += params.role + " LIKE \"" + params.person + "\" AND ";
+        }
+    } else if (params.person) {
+        params.person = params.person.replace("*", "%");
+        where += "(writer || penciller || inker || colorist || letterer || coverArtist) LIKE \"" + params.person + "\" AND ";
+    }
+
+    if (!isEmpty(params.keyphrase)) {
+        params.keyphrase = "%" + params.keyphrase.replace("*", "%") + "%";
+        where += "((volume || title || publisher || path || synopsis || notes || characters || storyArc) LIKE \"" + params.keyphrase + "\" AND ";
+    }
+
+    //NOTE: volume in YAC Reader and volume in ComicStreamer refer to two different values
+    //YAC Reader uses volume to refer to the series title
+    //ComicStreamer uses it to refer to the actual volume of the series (ie. The Amazing Spiderman Volume 2)
+    //YAC Reader doesn't have a value for the volume number so I'll just query for the storyArc
+    where += addQueryOnScalar("arcNumber", params.volume);
+    where += addQueryOnScalar("title", params.title);
+
+    where += addQueryOnScalar("comicPath", params.path);
+    where += addQueryOnScalar("folderPath", params.folder);
+    where += addQueryOnScalar("publisher", params.publisher);
+    //Characters are stored in a way that can't be .split so we'll just wildcard it :P
+    if (!isEmpty(params.characters)) {
+        params.characters = params.characters.replace("*", "%");
+        where += "characters LIKE \"" + params.characters + "\" AND ";
+    }
+    if (!isEmpty(params.tag)) {
+        params.tag = params.tag.replace("*", "%");
+        where += "(characters || synopsis || publisher || writer || coverArtist) LIKE " + params.tag + " AND ";
+    }
+
+    where += addQueryOnScalar("storyArc", params.storyarc);
+    where += addQueryOnScalar("genre", params.genre);
+
+    if (!isNaN(params.volume)) {
+        where += addQueryOnScalar("volume", params.volume); //the volume column is just an alias to the arcNumber column and will likely be null
+    }
+
+    //date parsing woo hoo
+    if (!isEmpty(params.start_date)) {
+        var date = moment(params.start_date);
+        if (date.isValid()) {
+            where += addQueryOnScalar("date", date.format("DD/MM/YYYY"));
+        }
+    }
+
+    //YAC Reader doesn't store any other dates so those can't be queried for :(
+
+
+    //Add the where
+    if (where.length > 0) {
+        sql += " WHERE " + where.slice(0, -5); //slices off the trailing "AND "
+    }
+
+    //Sorting order
+    if (!isEmpty(params.order)) {
+        var desc = (params.order.charAt(0) === "-"); //descending order if the first character is a '-'
+        var sortBy = "id";
+        params.order = params.order.substring(1);
+        switch (params.order) {
+            case "volume":
+            case "issue":
+            case "date":
+            case "publisher":
+            case "title":
+            case "path":
+            case "series":
+                sortBy = params.order;
+                break;
+            default:
+                sortBy = "id";
+        }
+
+        sql += " ORDER BY " + sortBy;
+        if (desc) {
+            sql += " DESC";
+        } else {
+            sql += " ASC";
+        }
+    }
+
+
+    //console.log("Querying with: " + sql);
     db.all(sql, function(err, rows) {
         if (err) {
             callback(err, null);
@@ -22,10 +122,22 @@ var getComicInfo = function(params, callback) {
         }
         for (var i = 0; i < rows.length; i++) {
             //Translate YAC Reader Metadata into ComicStreamer Compliant data
-            rows[i].series = rows[i].volume;
-            rows[i].added_ts = "";
-            rows[i].page_count = rows[i].numPages;
+            //Some of the fileinfo can't be retrieved without accessing the filesystem which would drastically slow down the performance (probably?)
+            rows[i].added_ts = moment();
             rows[i].locations = [];
+            rows[i].path = rows[i].comicPath;
+            rows[i].folder = rows[i].folderPath;
+            if (rows[i].storyArc !== null) {
+                rows[i].storyarcs = [rows[i].storyArc];
+            } else {
+            	rows[i].storyArc = [];
+            }
+
+            if(rows[i].genre !== null) {
+            	rows[i].genres =[rows[i].genre]
+            }
+            rows[i].genre += "";
+
 
             if (rows[i].date) {
                 var date = rows[i].date.split('/');
@@ -35,22 +147,35 @@ var getComicInfo = function(params, callback) {
                 rows[i].day = date[1];
             }
 
-            rows[i].deleted_ts = "";
-            rows[i].genres = [rows[i].genere];
+            rows[i].deleted_ts = moment();
+            //rows[i].genres = [rows[i].genere];
             rows[i].filesize = 0; //TODO: filesize
 
-            rows[i].issue = rows[i].number;
+            //rows[i].issue = rows[i].number;
             rows[i].creddits = {};
             rows[i].generictags = []; //I can probably put some of the tags/metadata ComicStreamer doesn't support but YACReader does in here
-            rows[i].lastread_ts = "";
+            rows[i].lastread_ts = moment();
             rows[i].teams = [];
 
+            //if anything is null cast it to an empty string
+            rows[i].series = rows[i].series === null ? "" : rows[i].series + "";
+            rows[i].issue = rows[i].issue === null ? "" : rows[i].issue + "";
+            rows[i].imprint = ""
+            rows[i].title = rows[i].title === null ? "" : rows[i].title + "";
+            rows[i].comments = rows[i].comments === null ? "" : rows[i].comments + "";
+            rows[i].issue = rows[i].issue === null ? "" : rows[i].comments + ""; 
+            rows[i].hash = rows[i].hash === null ? "" : rows[i].hash + "";
+            rows[i].lastread_page = rows[i].lastread_page === null ? "" : rows[i].lastread_page + "";
+            rows[i].weblink = "";
+            rows[i].volume = rows[i].volume === null ? "" : rows[i].volume;
+            rows[i].publisher = rows[i].publisher === null ? "" : rows[i].publisher;
         }
         callback(err, rows);
     });
 };
 
-var getComicFile = function(params, callback) {
+var getComicFile = function(id, callback) {
+	var sql = "SELECT path FROM comic WHERE id = " + id; 
     db.get(sql, callback);
 };
 
@@ -69,33 +194,38 @@ var getFolders = function(path, callback) {
             console.log("Error with path: " + err);
             return;
         }
-        db.all("SELECT id, parentId, name, path FROM folder where parentId=" + dir.id, function(err, dirsRows) {
+        db.all("SELECT id, parentId, name, path FROM folder where parentId=" + dir.id + " AND id != 1", function(err, dirsRows) {
             if (err) {
-            	callback(err, null); //requires testing. Might throw error if there's no children. Have to check
-            	console.log("Error with children dirs: " + err);
-            	return;
+                callback(err, null); //requires testing. Might throw error if there's no children. Have to check
+                console.log("Error with children dirs: " + err);
+                return;
             }
             var childrenDirs = [];
             for (var i = 0; i < dirsRows.length; i++) {
-            	childrenDirs.push({
-            		name: dirsRows[i],
-            		url_path: '/folders/0/' + dirsRows[i].path
-            	});
+                childrenDirs.push({
+                    name: dirsRows[i].name,
+                    url_path: '/folders/0/' + querystring.escape(dirsRows[i].path.substring(1)) //Substring because we can't escape the first /
+                });
             }
-            console.log(childrenDirs);
+            //console.log(childrenDirs);
 
             //NOTE: this isn't checking for comics in the children folders, only the directory at the path arg
             db.all("SELECT * from comic WHERE parentId = " + dir.id, function(err, comicsRows) {
-            	var result = {
-            		current: path,
-            		folders: childrenDirs,
-            		comics: {
-            			count: comicsRows.length,
-            			url_path: "/comiclist?folder=" + path //TODO: URL encode this
-            		}
-            	}
-            	console.log(result);
-            	callback(result);
+                if (err) {
+                    calback(err, null);
+                    //console.log("Error with children comics: " + err);
+                    return;
+                }
+                var result = {
+                        current: path,
+                        folders: childrenDirs,
+                        comics: {
+                            count: comicsRows.length,
+                            url_path: "/comiclist?folder=" + querystring.escape(path)
+                        }
+                    }
+                    //console.log(result);
+                callback(result);
             });
         })
     });
@@ -112,3 +242,24 @@ module.exports = {
     getFolders: getFolders
 
 };
+
+
+
+//Quick check to see if an we have an empty object
+function isEmpty(obj) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperties(prop))
+            return false;
+    }
+    return true;
+}
+
+//Adapted from ComicStreamer because it's a good time saver
+function addQueryOnScalar(column, filter) {
+    if (filter) {
+        filter = filter.replace(/\*/g, "%");
+        return column + " LIKE \"" + filter + "\" AND ";
+    } else {
+        return "";
+    }
+}
