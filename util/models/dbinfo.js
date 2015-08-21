871f1e7dedcf6067baa6e9f5cdaa0158c1c5c5b8
Noah Pederson <pedersonnoah@gmail.com>
2015-08-21 16:09:53 -0500

    Initial commit

---
+++ util/models/dbinfo.js
@@ -0,0 +1,6 @@
+//This is just a model for me to reference. Not actually used for anything
+{
+	mgaserver_version: "0.1",
+
+	yac_version: "8.0.0" //This will be empty or "0" if not using a yacreaderlibrary database
+}
